771c42f722be409b4abfe8e5eb230fa8cda80476
Sunny Aggarwal <sunnya97@gmail.com>
2018-01-08 18:14:18 -0800

    switched merkle implementation

--- client.js
+++ client.js
@@ -2,160 +2,129 @@ const assert = require('assert')
 const Web3 = require('web3')
 const ethJsUtil = require('ethereumjs-util')
 const merkle = require('merkle')
+var MerkleTools = require('merkle-tools')
 const stringify = require('json-stringify-deterministic')
 const contractAbi = require('./abi.json')
+const sha3_256 = require('js-sha3').sha3_256;
 
 var web3 = new Web3("http://localhost:8545");
 
 const attestchainContract = new web3.eth.Contract(contractAbi, "0xdd73bcaf6cd68884edf2be65afe0bc7241dc9fbb");
 
+var merkleTools = new MerkleTools({hashType: 'SHA3-256'});
+
 /*
 Specification
-	const signedSchema = mongoose.Schema({
+	const signedSchema = {
 		data: { type: String },
-		attestor_signature: { type: String },
-		merkle_proof: { type: String }
-		requester_signature: { type: String }
-	});
+		pubkey: { type: String },
+		signature: { type: String },
+		algorithm: { type: String },
+		merkleProof: { type: String }
+		merkleRoot: { type: String }
+	};
 
-	const dataSchema = mongoose.Schema({
+	const dataSchema = {
 		message: { type: String },
-		timestamp: { type: Number },
 		expiration: { type: Number },
 		isRevocable: { type: Boolean }
-	});
+	};
 */
 
 
 //When signing message is sent:
-//Data = array of signed objects
-const signing_handler = (data, private_key, revocable, signer) => {
-	let parsed_data = JSON.parse(data);
-	if(signer == 'requester')
-	{
-		for(let i = 0; i < parsed_data.length; i++)
-		{
-			parsed_data[i]['requester_signature'] = sign_attestation(parsed_data[i]['data'], private_key);
+//data_list = array of stringified data objects
+const signing_handler = (data_list, private_key) => {
+	
+	let signed_attestations = []
+	
+	let irrevocable_attestations = []
+
+	for(let i = 0; i < data_list.length; i++) {
+
+		signed_attestations.push(sign_attestation(data_list[i], private_key));
+
+		if(!JSON.parse(data_list[i]['isRevocable']) {
+			irrevocable_attestations.push(signed_attestations[i]);
 		}
 	}
-	else if (signer == 'attestor')
-	{
-		//sign all the messages
-		for(let i = 0; i < parsed_data.length; i++)
-		{
-			parsed_data[i]['attestor_signature'] = sign_attestation(parsed_data[i]['data'], private_key);
-		}
-		if(!revocable)
-		{
-			proofs = get_merkle_proofs(parsed_data);
-			for(let i = 0; i < parsed_data.length; i++)
-			{
-				if(parsed_data[i]['data']['isRevocable'])
-					throw("All entries must be irrevocable")
-				parsed_data[i]['merkle_proof'] = proofs[i];
-			}
-		}
-	}
-	else
-	{
-		throw("Signer must be a requester or attestor");
-	}
-	console.log(parsed_data);
-	return parsed_data;
+
+	add_merkle_proofs(irrevocable_attestations);
+
+	return signed_attestations;
 }
 
 
 const sign_attestation = (data, private_key) => {
-	let message = stringify(data);
-	//console.log(message);
-	const sig = web3.eth.accounts.sign(message, private_key);
-	const pub = ethJsUtil.bufferToHex(ethJsUtil.privateToPublic(private_key))
+	let pubkey = ethJsUtil.bufferToHex(ethJsUtil.privateToPublic(private_key)).toString();
+	let signature = web3.eth.accounts.sign(data, private_key)['signature'].toString();
 
-	//Deal with merkle proofing
-	//console.log(sig);
-	console.log("pub key: ");
-	console.log(pub);
+	return {
+		'data': data,
+		'pubkey': pubkey,
+		'signature': signature,
+		'algorithm': 'secp256k1'
+	};
+}
 
-	return sig['signature'];
-};
+const add_merkle_proofs = (irrevocable_attestations) => {
 
-const get_merkle_proofs = (data) => {
-	const length = data.length;
-	var leaves = [];
+	merkleTools.resetTree()
 
-	console.log("creating merkle tree and proofs")
-
-	for(var i = 0; i < length; i++) {
-		//TODO: verify validity of irrevocability
-		leaves.push(web3.utils.sha3(stringify(data[i])));
+	if(irrevocable_attestations.length == 0) {
+		return [];
 	}
 
-	for(var i = length; i < Math.pow(2, Math.ceil(Math.log2(length))); i++) {
-		var rand_seed = Math.random();
-		//console.log(rand_seed);
-		leaves.push(web3.utils.sha3(rand_seed.toString()));
+	for(var i = 0; irrevocable_attestations < length; i++) {
+		merkleTools.addLeaf(data_list[i] + signature, true);
 	}
 
-	console.log(leaves);
+	merkleTools.makeTree();
+	merkleroot = merkleTools.getMerkleRoot().toString('hex');
 
-	//Generate merkle trees
-	var tree = merkle('sha256').sync(leaves);
-
-	for(var i = 0; i < length; i++) {
-		//TODO: verify validity of irrevocability
-		//console.log(web3.utils.sha3(leaves[i]));
+	for(var i = 0; irrevocable_attestations < length; i++) {
+		irrevocable_attestations[i]['merkleRoot'] = merkleroot;
+		irrevocable_attestations[i]['merkleProof'] = stringify(getProof(i));
 	}
 
-	console.log(tree.level(tree.levels()-1))
+	return [merkleroot, irrevocable_attestations];
+}
 
-	//TODO: Send tree.root() to smart contract
 
-	var proofs = [];
-	//Return a list of merkle proofs
-	for(var i = 0; i < length; i++)
-	{
-		proofs.push(tree.getProofPath(i));
-	}
 
-	return proofs;
-};
-
-const verify_handler = (signed_attestation, attestor_pubkey, requester_pubkey, callback) => {
-	verify_attestation(signed_attestation, attestor_pubkey, requester_pubkey, callback);
+const verify_handler = (signed_attestation, check_pubkey, callback) => {
+	verify_attestation(signed_attestation, check_pubkey, callback);
 }
 
 const verify_signature = (data, sig, pubkey) => {
-	console.log(data);
 	recovered_addr = web3.eth.accounts.recover(stringify(data), sig).toLowerCase();
-	//console.log(recovered_addr);
 	check_addr = ethJsUtil.bufferToHex(ethJsUtil.pubToAddress(pubkey)).toLowerCase();
-	//console.log(check_addr);
 	return recovered_addr == check_addr;
 }
 
-const generate_merkle_root_from_proof = (merkle_proof, attestation_data) => {
-	let node = web3.utils.sha3(stringify(attestation_data));
-	let leaf_rehashed = ethJsUtil.bufferToHex(ethJsUtil.sha256(node));
-	console.log(leaf_rehashed);
-	console.log(merkle_proof);
-	return "";
-}
-
-const verify_attestation = (signed_attestation, attestor_pubkey, requester_pubkey, callback) => {
+const verify_attestation = (signed_attestation, check_pubkey, callback) => {
 	//Verify signers
-	const attestation_data = signed_attestation['data'];
-	if(!verify_signature(attestation_data, signed_attestation['attestor_signature'], attestor_pubkey)) {
+	let attestation = JSON.parse{sign_attestation};
+	let data = JSON.parse(attestation['data']);
+
+	if(attestation['algorithm'] != 'secp256k1') {
+		throw("algorithm currently not supported")
+	}
+
+	if (check_pubkey && check_pubkey != attestation['pubkey']) {
+		return false;
+	}
+
+	if(!verify_signature(attestation['data'], attestation['signature'], attestation['pubkey'])) {
 		callback("attestor signature invalid");
 		return false;
 	}
-	if(!verify_signature(attestation_data, signed_attestation['requester_signature'], requester_pubkey)) {
-		callback("requester signature invalid");
+
+	if(Date.now() > data['expiration']) {
 		return false;
 	}
 
-	//TODO: Verify timestamp is not expired
-
-	//Get the list of compromised signatures
+	//Get the list of compromised private keys
 	attestchainContract.methods.get_compromisation_list().call(function(error, response) {
 		if(error)
 		{
--- contracts/attestchain.sol
+++ contracts/attestchain.sol
@@ -9,12 +9,14 @@ contract AttestChain {
     struct Compromisation {
          bytes32 comp_priv_key;
          byte[64] new_pub_key;
+         uint block_num;
     }
 
 
     struct MerkleRoot {
          bytes32 merkle_root;
          byte[130] signature;
+         uint block_num;
     }
 
     Revocation[] public revocations;
@@ -30,11 +32,11 @@ contract AttestChain {
     }
 
     function compromise(bytes32 comp_priv_key, byte[64] new_pub_key) public {
-        compromisations.push(Compromisation(comp_priv_key, new_pub_key));
+        compromisations.push(Compromisation(comp_priv_key, new_pub_key, block.number));
     }
 
     function submit_root(bytes32 root, byte[130] signature) public {
-        merkleroots.push(MerkleRoot(root, signature));
+        merkleroots.push(MerkleRoot(root, signature, block.number));
     }
 
     function get_revocation_list() public constant returns (bytes32[], byte[130][]) {
@@ -49,27 +51,32 @@ contract AttestChain {
         return (hashArr, sigArr);
     }
 
-    function get_compromisation_list() public constant returns (bytes32[], byte[64][]) {
+    function get_compromisation_list() public constant returns (bytes32[], byte[64][], uint[]) {
         bytes32[] memory privKeyArr = new bytes32[](compromisations.length);
         byte[64][] memory pubKeyArr = new byte[64][](compromisations.length);
+        uint[] memory blockNumArr = new uint[](compromisations.length);
 
         for (uint i = 0; i < compromisations.length; i++) {
              privKeyArr[i] = compromisations[i].comp_priv_key;
              pubKeyArr[i] = compromisations[i].new_pub_key;
+             blockNumArr[i] = compromisations[i].block_num;
         }
 
-        return (privKeyArr, pubKeyArr);
+        return (privKeyArr, pubKeyArr, blockNumArr);
     }
 
-    function get_merkleroots_list() public constant returns (bytes32[], byte[130][]) {
+    function get_merkleroots_list() public constant returns (bytes32[], byte[130][], uint[]) {
         bytes32[] memory rootArr = new bytes32[](merkleroots.length);
         byte[130][] memory sigArr = new byte[130][](merkleroots.length);
+        uint[] memory blockNumArr = new uint[](merkleroots.length);
+
 
         for (uint i = 0; i < merkleroots.length; i++) {
              rootArr[i] = merkleroots[i].merkle_root;
              sigArr[i] = merkleroots[i].signature;
+             blockNumArr[i] = merkleroots[i].block_num;
         }
 
-        return (rootArr, sigArr);
+        return (rootArr, sigArr, blockNumArr);
     }
 }
